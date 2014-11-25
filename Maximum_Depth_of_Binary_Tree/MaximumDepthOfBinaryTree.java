52851e2ac49b37296ca196f895a849c7f773da5a
Guo-min-zhi <eric.minzhi.guo@gmail.com>
2014-11-25 23:14:03 +0800

    Maximum Depth of Binary Tree of java

--- Maximum_Depth_of_Binary_Tree/MaximumDepthOfBinaryTree.java
+++ Maximum_Depth_of_Binary_Tree/MaximumDepthOfBinaryTree.java
@@ -11,7 +11,7 @@ public class Solution {
     public int maxDepth(TreeNode root) {
         if(root == null){
             return 0;
-        }
+        } 
         int left = maxDepth(root.left);
         int right = maxDepth(root.right);
         return left>right?left+1:right+1;
