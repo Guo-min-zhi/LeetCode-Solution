1745c6cdc90906e830f564ee8a333589cb19e532
Guominzhi <eric.minzhi.guo@gmail.com>
2014-11-26 09:34:14 +0800

    binary tree inorder traversal of java, recursive version

---
+++ Binary_Tree_Inorder_Traversal/BinaryTreeInorderTraversal.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution{
+    /**
+    * Recursive Version
+    */
+    public List<Integer> inorderTraversal(TreeNode root){
+        List<Integer> list = new ArrayList<Integer>();
+        if (root != null) {
+            list.addAll(inorderTraversal(root.left));
+            list.add(root.val);
+            list.addAll(inorderTraversal(root.right));
+        }
+        return list;
+    }
+}
